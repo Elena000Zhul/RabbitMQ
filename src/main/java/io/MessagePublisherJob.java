fa7922a1e4c38e28aebe953aa5d0cc97c65d5792
Elena000Zhul <elena000zhul@gmail.com>
2024-12-19 14:41:57 +0100

    update

--- src/main/java/io/MessagePublisherJob.java
+++ src/main/java/io/MessagePublisherJob.java
@@ -1,20 +1,20 @@
 package io;
 
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-public class MessagePublisherJob implements Job {
+@Component
+public class MessagePublisherJob  {
+
     @Autowired
-    private AmqpTemplate amqpTemplate;
-
+    private RabbitTemplate amqpTemplate;
     private static final String EXCHANGE_NAME = RabbitConfig.EXCHANGE_NAME;
     private static final String ROUTING_KEY = RabbitConfig.ROUTING_KEY;
 
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    @Scheduled(fixedRate = 100)
+    protected void executeInternal(){
         String message = "Message " + System.currentTimeMillis();
         amqpTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, message);
         System.out.println("Published message: " + message);
